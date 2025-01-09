73df6364df244ef58cf8b59a4281d229ea820d72
dev2 better <506343891@qq.com>
2025-01-09 21:05:04 +0800

    111测试

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,38 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Navigation.css';
 
 const Navigation = () => {
   return (
-    <nav className="main-navigation">
-      <ul>
-        <li>
-          <Link to="/" className="nav-link">
-            HOME
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className="nav-link">
-            ABOUT
-          </Link>
-        </li>
-        <li>
-          <Link to="/portfolio" className="nav-link">
-            PORTFOLIO
-          </Link>
-        </li>
-        <li>
-          <Link to="/tags" className="nav-link">
-            TAGS
-          </Link>
-        </li>
-        <li>
-          <Link to="/write" className="nav-link">
-            WRITE
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <Link to="/">HOME</Link>
+        <Link to="/about">ABOUT</Link>
+        <Link to="/portfolio">PORTFOLIO</Link>
+        <Link to="/tags">TAGS</Link>
+        <Link to="/write">WRITE</Link>
+      </nav>
+    </header>
   );
 };
 
