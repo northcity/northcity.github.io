4bfe8c07fb96feacbe5f08f206e2d41731293e43
dev2 better <506343891@qq.com>
2025-01-10 10:00:27 +0800

    1

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <header>
-      <nav>
+    <div className="header">
+      <div className="header-inner">
         <Link to="/">HOME</Link>
         <Link to="/about">ABOUT</Link>
         <Link to="/portfolio">PORTFOLIO</Link>
         <Link to="/tags">TAGS</Link>
         <Link to="/write">WRITE</Link>
-      </nav>
-    </header>
+      </div>
+    </div>
   );
 };
 
